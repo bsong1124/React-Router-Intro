6c3db023b879b3ba3ada490e8713d49a06ae6b7d
bsong1124 <bsong1124@gmail.com>
2023-12-08 11:38:09 -0500

    change file structure and add Nav component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,21 @@
-import { useState } from 'react'
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
 
+import Main from '../../pages/Main/Main'
+import StarshipsList from '../../pages/StarshipsList/StarshipsList'
+import Starship from '../../pages/Starship/Starship'
+import Nav from "../Nav/Nav"
 function App() {
   return (
     <div className='App'>
-      <h1>React Router Intro</h1>
+      <Nav />
+      <main className='container'>
+        <Routes>
+          <Route path='/' element={<Main />} />
+          <Route path='/ships' element={<StarshipsList />} />
+          <Route path='/ships/:id' element={<Starship />} />
+        </Routes>
+      </main>
     </div>
   )
 }
